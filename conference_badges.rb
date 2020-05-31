c828cfe5ceb0d9e64c9c11b814934b9005fe7192
Kayla Kingston <kkingston09@gmail.com>
2020-05-31 18:28:29 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def badge_maker(person)
+  return "Hello, my name is #{person}."
+end
+
+speakers = ['Edsger', 'Ada', 'Charles', 'Alan', 'Grace', 'Linus', 'Matz']
+
+def batch_badge_creator(speakers)
+ speakers.collect {|name| badge_maker(name)}
+end
+
+def assign_rooms(speakers)
+  room = speakers.each_with_index
+  
+  speakers.collect {|name| puts "Hello #{name}! You will be assigned to room #{room}!"
+end
+
+
